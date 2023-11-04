49258e8188f7f1dda99c255ba0bb1449d3440e20
Moesha <moesha.touzeyidio@gmail.com>
2023-11-04 20:43:17 +0100

    feat(main): add the principal method of the project

---
+++ main.py
@@ -0,0 +1,10 @@
+from tictactoe.tictactoe_game import TictactoeGame
+from tictactoe.tictactoe_gui import TictactoeGui
+
+def main():
+    game = TictactoeGame()
+    TictactoeGui(game)
+
+
+if __name__ == "__main__":
+    main()
